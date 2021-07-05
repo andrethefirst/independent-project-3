85a09df92cc31511af980440ba0f2a96535548aa
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-05 13:11:47 -0700

    add Utility Logic and basic format for index

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 // Utility Logic
-
-
-
-
-
-
-
-
-
+function noInputtedWord() {
+  for (let i=0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+    if (arguments[i].trim().length === 0) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // Business Logic 
 
+function beepBoop(text) { 
+  
+}
 
 
 
