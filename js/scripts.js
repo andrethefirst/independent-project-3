644bf88960e140990094ad073f97acfd2a0b6e1e
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-05 14:36:45 -0700

    add beepBoop working function

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 // Utility Logic
-function noInputtedWord() {
-  for (let i=0; i < arguments.length; i++) {
-    console.log(arguments[i]);
-    if (arguments[i].trim().length === 0) {
-      return true;
+  function noInputtedWord() {
+    for (let i=0; i < arguments.length; i++) {
+      console.log(arguments[i]);
+      if (arguments[i].trim().length === 0) {
+        return true;
+      }
     }
+    return false;
   }
-  return false;
-}
 
 // Business Logic 
 
-function beepBoop(text) { 
-  
+function beepBoop(inputNumber) {
+  let numberedArray = [];
+  for(let i = 0; i <= inputNumber; i++) {
+    i = i.toString();
+    if(i.includes(3)) {
+      numberedArray.push("Won't you be my neighbor?")
+    }else if(i.includes(2)) {
+      numberedArray.push("Boop!")
+    }else if(i.includes(1)){
+      numberedArray.push("Beep!")
+    }else {
+      numberedArray.push(i); 
+    }
+  }
+  return numberedArray;
 }
+
 
 
 
